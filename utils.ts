674d5793786f46ded6838b25c0875de0758bea13
Prince527 <Prince527gaming@gmail.com>
2024-07-14 18:25:16 -0300

    Add files via upload

--- utils.ts
+++ utils.ts
@@ -38,9 +38,13 @@ export interface TranslationValue {
 const dictonary = /\b(?:leko|weka|pan|lete|linja|lipu|suli|nimi|akesi|misikeke|selo|ike|sijelo|sona|lili|pimeja|ante|jo|loje|telo|walo|kijetesantakalu|kasi|waso|wile|utala|lukin|sina|lape|ma|pilin|jasima|la|olin|pipi|meso|lawa|pi|pakala|oko|tan|ken|jaki|unpa|esun|seme|sitelen|len|kule|soko|open|ala|tenpo|lon|sinpin|pini|kokosila|mama|musi|monsi|mewika|taso|ona|mun|kiwen|tomo|mute|mi|nena|palisa|meli|laso|wawa|ale|kipisi|kulupu|ilo|lupa|nanpa|en|mu|jelo|kili|tonsi|moku|ni|kama|pu|poki|monsuta|sin|lasina|poka|soweli|sewi|elena|epiku|moli|pona|lanpan|alasa|anu|kute|uta|luka|suno|sama|awen|namako|suwi|noka|seli|mije|sike|jan|pali|tawa|inli|nasa|mani|wan|insa|nijon|nasin|kalama|ijo|toki|anpa|kala|kepeken|ko|kon|pana|tu|supa|kin|usawi|yupekosi)\b/gm;
 
 function isTokiPona(string) {
-    const matches = string.match(dictonary);
+    const words = string.split(/\s+/);
 
-    return matches && matches.length >= 50;
+    const matches = string.match(dictonary) || [];
+
+    const percentage = (matches.length / words.length) * 100;
+
+    return percentage >= 50;
 }
 
 export async function translate(kind: "received" | "sent", text: string): Promise<TranslationValue> {
